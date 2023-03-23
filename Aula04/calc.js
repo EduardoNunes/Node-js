858c2e865e208ce9b30dc902594d6f5bf5d1625c
Eduardo Lago <eduardolagonunes@gmail.com>
2023-03-23 19:05:06 -0300

    aula04 modulo

---
+++ Aula04/calc.js
@@ -0,0 +1,9 @@
+var somaF = require("./soma");
+var subtrairF = require("./subtrair");
+var multiplicarF = require("./multiplicar");
+var dividirF = require("./dividir");
+
+console.log(somaF(1,2));
+console.log(subtrairF(2,2));
+console.log(multiplicarF(3,2));
+console.log(dividirF(4,2));
