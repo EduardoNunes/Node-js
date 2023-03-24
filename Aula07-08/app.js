d78f02ce32e69f9b145de65f0362e1a9944a87c2
Eduardo Lago <eduardolagonunes@gmail.com>
2023-03-24 12:40:30 -0300

    Aula08 parametros

---
+++ Aula07-08/app.js
@@ -0,0 +1,24 @@
+//criando rotas usando express e passando parâmetros
+
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Seja bem vindo ao meu app!");
+});
+
+app.get("/sobre", function(req, res){
+    res.send("Minha página sobre");
+});
+
+app.get("/blog", function(req, res){
+    res.send("Seja bem vindo ao meu blog!");
+});
+
+app.get("/ola/:nome/:rico/:cor", function(req, res){
+    res.send('<h1>Ola ' + req.params.nome+"</h1>" + '<h2>Seu cargo e ' + req.params.rico+"</h2>" + '<h3>Sua cor favorita e ' + req.params.cor+"</h3>");
+});
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081")
+});
